af74f107e00be9a3d6d0035d4db74c6469673b65
Andrej Petras <andrej@lorislab.org>
2024-01-18 19:33:23 +0100

    tests: add tenant extension tests

--- extensions/tenant/src/main/java/io/github/onecx/quarkus/tenant/OnecxTenantService.java
+++ extensions/tenant/src/main/java/io/github/onecx/quarkus/tenant/OnecxTenantService.java
@@ -9,7 +9,8 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.github.onecx.quarkus.tenant.client.OnecxTenantClientRestClient;
+import gen.io.github.onecx.tenant.client.api.TenantV1Api;
+import gen.io.github.onecx.tenant.client.model.TenantId;
 import io.quarkus.arc.Unremovable;
 import io.quarkus.cache.CacheResult;
 
@@ -21,41 +22,44 @@ public class OnecxTenantService {
 
     @Inject
     @RestClient
-    OnecxTenantClientRestClient client;
+    TenantV1Api client;
 
-    @SuppressWarnings("java:S2139")
-    @CacheResult(cacheName = "onecx-tenant")
+    @Inject
+    TenantConfig config;
+
     public String getTenant(String token) {
+        if (config.cacheEnabled()) {
+            return getTenantCache(token);
+        }
+        return getTenantLocal(token);
+    }
 
-        try (Response response = client.getTenantId()) {
+    @CacheResult(cacheName = "onecx-tenant")
+    public String getTenantCache(String token) {
+        return getTenantLocal(token);
+    }
+
+    public String getTenantLocal(String token) {
+
+        try (Response response = client.getTenantMapsByOrgId()) {
             if (response.getStatus() == Response.Status.OK.getStatusCode()) {
-                TenantIdDTOV1 data = response.readEntity(TenantIdDTOV1.class);
+                TenantId data = response.readEntity(TenantId.class);
                 return data.getTenantId();
             }
 
             var body = response.readEntity(String.class);
-            log.error("Wrong response to get tenant-id from service. Status: {}, Entity: {}", response.getStatus(), body);
+            log.debug("Wrong response to get tenant-id from service. Status: {}, Entity: {}", response.getStatus(), body);
 
             throw new OnecxTenantException(ErrorKeys.ERROR_TENANT_SERVICE_WRONG_RESPONSE,
-                    "Wrong response to get tenant-id from service. Status: " + response.getStatus() + ", Message: " + body);
+                    "Wrong response to get tenant-id from service. Status: " + response.getStatus());
 
         } catch (WebApplicationException ex) {
             var body = ex.getResponse().readEntity(String.class);
-            log.error("Error get tenant-id from service. Status: {}, Message: {}", ex.getResponse().getStatus(), body, ex);
+            log.error("Error get tenant-id from service. Status: {}, Message: {}", ex.getResponse().getStatus(), body);
 
             throw new OnecxTenantException(ErrorKeys.ERROR_TENANT_SERVICE_EXCEPTION_RESPONSE,
-                    "Error get tenant-id from service. Status: " + ex.getResponse().getStatus() + ", Message: " + body,
-                    ex);
+                    "Error get tenant-id from service. Status: " + ex.getResponse().getStatus());
         }
-    }
-
-    public static class TenantIdDTOV1 {
-        private String tenantId;
-
-        public String getTenantId() {
-            return tenantId;
-        }
-
     }
 
     public enum ErrorKeys {

df1c48bcd7d522c403f71b960fa5b12c1f1166b5
Andrej Petras <andrej@lorislab.org>
2025-05-14 14:35:04 +0200

    feat: use always local parameters

--- extensions/parameters/runtime/src/main/java/org/tkit/onecx/quarkus/parameter/metrics/NoopMetricsRecorder.java
+++ extensions/parameters/runtime/src/main/java/org/tkit/onecx/quarkus/parameter/metrics/NoopMetricsRecorder.java
@@ -1,8 +1,5 @@
 package org.tkit.onecx.quarkus.parameter.metrics;
 
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
 public class NoopMetricsRecorder implements MetricsRecorder {
 
     public void increase(String name) {

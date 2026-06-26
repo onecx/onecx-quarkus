301dff535ad065e858e2a5216f41a838d12cffe0
Michal Somora <10022003+michalsomora@users.noreply.github.com>
2026-06-26 10:54:25 +0200

    fix(config): propagate headers for MicroProfile REST client (#372)

--- extensions/parameters/client/src/main/java/org/tkit/onecx/quarkus/parameters/client/HeadersFactory.java
+++ extensions/parameters/client/src/main/java/org/tkit/onecx/quarkus/parameters/client/HeadersFactory.java
@@ -3,11 +3,15 @@ package org.tkit.onecx.quarkus.parameters.client;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.MultivaluedMap;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.ext.DefaultClientHeadersFactoryImpl;
 import org.tkit.quarkus.context.ApplicationContext;
 
 @ApplicationScoped
 public class HeadersFactory extends DefaultClientHeadersFactoryImpl {
+
+    @ConfigProperty(name = "onecx.parameters.token-header-param")
+    String tokenHeaderParam;
 
     @Override
     public MultivaluedMap<String, String> update(MultivaluedMap<String, String> incomingHeaders,
@@ -15,10 +19,11 @@ public class HeadersFactory extends DefaultClientHeadersFactoryImpl {
         MultivaluedMap<String, String> tmp = super.update(incomingHeaders, clientOutgoingHeaders);
 
         var ctx = ApplicationContext.get();
+
         if (ctx != null) {
             var token = ctx.getPrincipalToken();
-            if (token != null) {
-                tmp.putSingle("apm-principal-token", token.getRawToken());
+            if (token != null && !tmp.containsKey(tokenHeaderParam)) {
+                tmp.putSingle(tokenHeaderParam, token.getRawToken());
             }
         }
         return tmp;
